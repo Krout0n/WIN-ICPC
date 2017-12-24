7ea757f669633a6d6dcb70245839ed7a6fd85548
Passawee Jantakorn <passawee.jan@gmail.com>
2017-12-24 09:46:41 +0900

    rmv print

--- abc/083/C.py
+++ abc/083/C.py
@@ -1,5 +1,4 @@
 def rec(result, i, dest, step):
-    print('i > dest: ', i, dest)
     if i > dest:
         return result
     if i % result[-1] == 0:
@@ -7,7 +6,7 @@ def rec(result, i, dest, step):
         rec(result, i+result[-1], dest, result[-1])
     else:
         rec(result, i+step, dest, result[-1])
-        
+
 x, y = [int(i) for i in input().split()]
 l = [x]
 ans = rec(l, 2*x, y, x)
