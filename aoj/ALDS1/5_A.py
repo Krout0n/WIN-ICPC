2bd72bb8cbe3c0a531a5fafc4a61f1bd5a53f3a2
Knium <hi.im.knium@gmail.com>
2017-12-30 18:54:00 +0900

    なんかRuntimeError吐いた

---
+++ aoj/ALDS1/5_A.py
@@ -0,0 +1,13 @@
+input()
+a = [int(i) for i in input().split()]
+
+def solve(i, m):
+    if m == 0:
+        return True
+    if i >= len(a):
+        return False
+    return solve(i+1, m) or solve(i+1, m - a[i]) # m - a[i] でその数字を使ったかどうか判定する
+
+input()
+for i in input().split():
+    print('yes') if solve(0, int(i)) else print('no')
