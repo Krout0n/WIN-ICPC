4a9a20ac100e1b2870c393927bb4e64c33dd6e02
Knium <hi.im.knium@gmail.com>
2018-01-01 14:38:37 +0900

    solved

---
+++ aoj/Vol.0-3/0039.py
@@ -0,0 +1,28 @@
+import sys
+
+d = {
+    'I': 1,
+    'V': 5,
+    'X': 10,
+    'L': 50,
+    'C': 100,
+    'D': 500,
+    'M': 1000
+}
+for s in sys.stdin:
+    c = 0
+    for i,j in enumerate(list(s)):
+        if j == '\n':
+            break
+        try:
+            if j == 'I' and (s[i+1] == 'V' or s[i+1] == 'X'):
+                c -= 1
+            elif j == 'X' and (s[i+1] == 'L' or s[i+1] == 'C'):
+                c -= 10
+            elif j == 'C' and (s[i+1] == 'D' or s[i+1] == 'M'):
+                c -= 100
+            else:
+                c += d[j]
+        except:
+            c += d[j]
+    print(c)
